14eb49676f7245d3eefc25cac50553142564dd21
v1mirza <mirzaone@proton.me>
2024-06-13 19:38:44 +0530

    Added JavaScript folder

---
+++ JavaScript/conversion.js
@@ -0,0 +1,23 @@
+// console.log("Hello world");
+
+// const accountId = "1234"
+// let accountPass = "1111"
+// let accountEmail = "hello@hello.com"
+
+
+// // console.log(accountId)
+// console.log([accountId, accountPass, accountEmail]);
+
+
+
+// data type conversion
+
+let num =  124
+let newValue = num
+console.log(newValue);
+
+console.log(typeof newValue);
+
+let newNum = String(n)
+console.log(newNum);
+console.log(typeof newNum);
---
+++ JavaScript/variable.js
@@ -0,0 +1,12 @@
+// console.log("Hello world");
+
+const accountId = "1234"
+let accountPass = "1111"
+var accountEmail = "hello@hello.com"
+
+
+console.log(accountId)
+console.log([accountId, accountPass, accountEmail]);
+
+
+
