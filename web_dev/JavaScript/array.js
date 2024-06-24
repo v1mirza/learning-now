0a82d515c8dcf270239de93ec4d6586e09443581
v1mirza <mirzaone@proton.me>
2024-06-25 02:52:15 +0530

    Added Array.js in JavaScript

---
+++ web_dev/JavaScript/array.js
@@ -0,0 +1,28 @@
+// Arrays
+
+let myArr = [1,2,3,4]
+
+let myArr2 = ["Ironman", "Batman", "Joker"]
+
+// Array's indexing start frm zero
+console.log(myArr[1]);
+console.log(myArr2[0]);
+
+
+// Array methods
+
+// To add another value in Array
+myArr.push(5)
+console.log(myArr);
+
+// To remove last value
+myArr.pop()
+console.log(myArr); 
+
+// These methods will answer in true or false
+console.log(myArr.includes(7));
+console.log(myArr.indexOf(4));
+
+ 
+// To convert array into string
+console.log(myArr.join());
