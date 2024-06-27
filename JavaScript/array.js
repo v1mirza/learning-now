cb37e0359cc7d62986fe4910e175bf49df9d40a9
v1mirza <mirzaone@proton.me>
2024-06-27 21:51:35 +0530

    updated array.js

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -24,5 +24,28 @@ console.log(myArr.includes(7));
 console.log(myArr.indexOf(4));
 
  
-// To convert array into string
+// To convert array into   string
 console.log(myArr.join());
+
+
+// Merging Arrays
+
+let marvel_heros = ["Ironman", "Captain", "Spiderman"]
+let dc_heros = ["Batman", "Superman", "Wonderwomen"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// let allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+let all_new_heros =  [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
+
